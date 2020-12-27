1989f88a6ee29f97a62db6ef8af36b6926816b03
jerebustos <jerembustos@gmail.com>
2020-12-27 20:36:40 -0300

    cambien nombre de carpeta y agregue login

---
+++ Web/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
