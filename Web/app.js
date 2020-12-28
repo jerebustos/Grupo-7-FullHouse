c19841b1bc6b90851a751a51ca4910231967f3ac
jerebustos <jerembustos@gmail.com>
2020-12-27 21:02:25 -0300

    Probando

--- Web/app.js
+++ Web/app.js
@@ -15,3 +15,7 @@ app.get('/', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
