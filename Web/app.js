d7e2d169b5fd937412ec04b08e89e374824d1510
jerebustos <jerembustos@gmail.com>
2021-01-25 18:18:27 -0300

    agregue carpeta views

--- Web/app.js
+++ Web/app.js
@@ -10,21 +10,21 @@ app.listen(3000, ()=>{
 });
 
 app.get('/', (req,res)=>{
-       res.sendFile(__dirname + '/index.html');
+       res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/login.html');
+    res.sendFile(__dirname + '/views/login.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/register.html');
+    res.sendFile(__dirname + '/views/register.html');
 });
 
 app.get('/carrito', (req,res)=>{
-    res.sendFile(__dirname + '/productCart.html');
+    res.sendFile(__dirname + '/views/productCart.html');
 });
 
 app.get('/producto', (req,res)=>{
-    res.sendFile(__dirname + '/productDetail.html');
+    res.sendFile(__dirname + '/views/productDetail.html');
 });
