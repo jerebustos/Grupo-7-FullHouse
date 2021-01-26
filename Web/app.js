3e9b73484b4f6f9d23a77ad3d35765679b4de810
jerebustos <jerembustos@gmail.com>
2021-01-26 20:39:37 -0300

    instalando ejs

--- Web/app.js
+++ Web/app.js
@@ -1,16 +1,21 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 
 app.listen(3000, ()=>{
-    console.log(__dirname + ' Servidor funcionando ');
+    console.log(' Servidor funcionando ');
 });
 
 app.get('/', (req,res)=>{
-       res.sendFile(__dirname + '/views/index.html');
+       res.render("index");
 });
 
 app.get('/login', (req,res)=>{
