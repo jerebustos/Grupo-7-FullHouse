12ec3ea7f1ce67dd5f6c07c47c6831671fc7115e
jerebustos <jerembustos@gmail.com>
2021-04-03 18:38:56 -0300

    acomodando pagina de inicio

--- Web/controllers/mainController.js
+++ Web/controllers/mainController.js
@@ -9,9 +9,21 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 index:(req,res)=>{
+    const ProductosGrandes = [];
+    const ProductosPequeños  = [];
+
+    products.map(producto => {
+        if(producto.category == "grande"){
+            ProductosGrandes.push(producto)
+         }
+         else if(producto.category == "pequeño"){
+            ProductosPequeños.push(producto)
+         }
+    })
 
     const paraLaVista = {
-        products,
+        pequeños : ProductosPequeños ,
+        grandes  : ProductosGrandes,
         toThousand
     }
     res.render("index",paraLaVista);
