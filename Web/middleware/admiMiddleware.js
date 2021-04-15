fe3e88aad3fd3186cf8f9b9b0a52fbfae1f6c333
jerebustos <jerembustos@gmail.com>
2021-04-15 11:24:08 -0300

    si esta logeado y no sos admi te manda a index

--- Web/middleware/admiMiddleware.js
+++ Web/middleware/admiMiddleware.js
@@ -1,9 +1,14 @@
 function admiMiddleware(req, res, next) {
 
-	if (!req.session.userLogged || req.session.userLogged.admi != 1) {
+	if (!req.session.userLogged ) {
 		return res.redirect('/usuario/ingresar');
 	}
-	next();
+	
+    if (req.session.userLogged.admi != 1) {
+        return res.redirect("/")
+        
+    }
+    next();
 }
 
 module.exports = admiMiddleware;
