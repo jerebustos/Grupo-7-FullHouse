c566d04ecc0124f7d74e388fe5521f14fb1ee233
jerebustos <jerembustos@gmail.com>
2021-04-15 11:13:38 -0300

    acomodando admiMiddleware ahora te manda a logear para acceder a rutas admis

--- Web/middleware/admiMiddleware.js
+++ Web/middleware/admiMiddleware.js
@@ -1,7 +1,7 @@
 function admiMiddleware(req, res, next) {
 
 	if (!req.session.userLogged || req.session.userLogged.admi != 1) {
-		return res.redirect('/');
+		return res.redirect('/usuario/ingresar');
 	}
 	next();
 }
